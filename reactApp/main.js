c513133437ef7b4c5ab05298352eeda6afbb0293
Vartika Singh <vrsingh@grepruby.com>
2017-08-17 17:14:51 +0530

    Added main.js

---
+++ reactApp/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app1.jsx';
+import Child from './child.jsx'
+ReactDOM.render(<App />, document.getElementById('app'));
