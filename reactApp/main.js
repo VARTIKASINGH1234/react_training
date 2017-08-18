5eb6fe7c4b0f079cddaaad0e2c5dbcc5ee87256d
Vartika Singh <vrsingh@grepruby.com>
2017-08-18 12:42:57 +0530

    Made changes to main.js

--- reactApp/main.js
+++ reactApp/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app1.jsx';
+import Parent from './parent.jsx';
 import Child from './child.jsx'
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Parent />, document.getElementById('app'));
