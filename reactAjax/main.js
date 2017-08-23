4c4afc3f57ca7a3478e6c5955751a694bef47e0b
Vartika Singh <vrsingh@grepruby.com>
2017-08-23 20:15:21 +0530

    Added main.js

---
+++ reactAjax/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Parent from './parent3.jsx';
+import List from './list.jsx';
+ReactDOM.render(<List />, document.getElementById('app'));
