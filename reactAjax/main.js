3ea41b1b5c386fed73300216647e230e9b4db4cd
Vartika Singh <vrsingh@grepruby.com>
2017-08-24 16:07:01 +0530

    Modified main.js

--- reactAjax/main.js
+++ reactAjax/main.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Parent from './parent3.jsx';
-import List from './list.jsx';
-ReactDOM.render(<List />, document.getElementById('app'));
+import Parent from './parent.jsx';
+ReactDOM.render(<Parent />, document.getElementById('app'));
