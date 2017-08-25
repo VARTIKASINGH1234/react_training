62981149ab02b76226e3d50ba46d2cda99bdbb68
Vartika Singh <vrsingh@grepruby.com>
2017-08-25 19:57:22 +0530

    Added addList.jsx

---
+++ reactRouter/addList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './list.jsx';
+import request from 'sync-request';
+import ClientList from './clientList.jsx';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute } from 'react-router'
+
+
+class AddList extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+
+  handleClick (event) {
+    let input = this.refs.myInput.value;
+    var res = request('POST', 'http://localhost:8081/clients', {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: input
+    });
+    this.refs.myInput.value = '';
+    ReactDOM.findDOMNode(this.refs.myInput).focus();
+    window.location.reload();
+  }
+
+
+  render() { 
+    return (
+      <div>
+        <textarea  cols="40" rows="5" ref = "myInput"></textarea>
+        <button type="button" onClick={this.handleClick}>Post Clients</button> 
+      </div>
+    );
+  }
+}
+
+
+export default AddList;
+
