3e47ba4d65b08f0115979c397d0861f9d453e75e
Vartika Singh <vrsingh@grepruby.com>
2017-08-25 20:00:36 +0530

    Added list.jsx

---
+++ reactRouter/list.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const List = ({clientList}) => (
+  <ul>
+    {clientList.length &&clientList.map((item,index) => 
+      <li key={index}>{item.Name}</li>
+    )}      
+  </ul>
+);
+
+
+  List.propTypes = {
+    clientList: PropTypes.array.isRequired,
+  };
+
+export default List
