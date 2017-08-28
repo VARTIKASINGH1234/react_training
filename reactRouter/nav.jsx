2f4ea06e687d54cb2649adb238f6737d5150c585
Vartika Singh <vrsingh@grepruby.com>
2017-08-28 15:41:44 +0530

    Modified nav.jsx using navigate Function

--- reactRouter/nav.jsx
+++ reactRouter/nav.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router'
+import {Link, hashHistory} from 'react-router'
 
-const Nav = () => (
-  <div>
-    <Link to = '/'>Home</Link>&nbsp;
-    <Link to = '/lists'>Get Clients</Link>&nbsp;
-    <Link to = '/addLists'>Create Clients</Link>
-  </div>
-)
+class Nav extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onNavigateHome = this.onNavigateHome.bind(this);
+    this.onNavigateList = this.onNavigateList.bind(this);
+    this.onNavigateAddList = this.onNavigateAddList.bind(this);
+  }
+
+
+  onNavigateHome(event) {
+    hashHistory.push('/');
+  }
+
+
+  onNavigateList(event) {
+    hashHistory.push('/lists');
+  }
+
+
+  onNavigateAddList(event) {
+    hashHistory.push('/addLists');
+  }
+
+  render() {
+    return (
+      <div>
+    	<button type="button" onClick={this.onNavigateHome}>Home</button>&nbsp; 
+    	<button type="button" onClick={this.onNavigateList}>Get Clients</button>&nbsp; 
+    	<button type="button" onClick={this.onNavigateAddList}>Create Clients</button> 
+ 	  </div>
+ 	);
+  }
+}
 
 
 export default Nav;
