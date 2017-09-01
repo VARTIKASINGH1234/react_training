c453784ac460f85061645c182b9cae5424379e01
Vartika Singh <vrsingh@grepruby.com>
2017-09-01 12:29:13 +0530

    Modified dev/js/index.js

--- reduxApp2/dev/js/index.js
+++ reduxApp2/dev/js/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import allReducers from './reducers';
 import App from './components/App';
