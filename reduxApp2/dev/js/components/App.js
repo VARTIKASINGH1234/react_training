2c2ccd273953d9e651b12d37aa1e76c2380c0da5
Vartika Singh <vrsingh@grepruby.com>
2017-09-01 12:27:28 +0530

    Modified  dev/js/components/App.js

--- reduxApp2/dev/js/components/App.js
+++ reduxApp2/dev/js/components/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import FillDetails from '../containers/fill-details';
-import UserDetails from '../containers/user-details';
-require('../../scss/style.scss');
-
+import FillUserDetails from '../containers/fill-userDetails';
+import DisplayUserDetails from '../containers/display-userDetails';
+import style from '../../scss/style.scss';
 
 const App = () => (
   <div>
     <h2>Fill your Details</h2>
-    <FillDetails />
+    <FillUserDetails />
     <hr />
-    <UserDetails />
+    <DisplayUserDetails />
   </div>
 );
-
 
 export default App;
